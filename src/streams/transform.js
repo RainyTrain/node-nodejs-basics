f20a423c123407946f58e20f915fa1e5df426926
Adrian Senkevich <adrianscorp@gmail.com>
2024-01-30 14:45:48 +0100

    resolved stream module

--- src/streams/transform.js
+++ src/streams/transform.js
@@ -1,5 +1,23 @@
-const transform = async () => {
-    // Write your code here 
+import { Transform } from 'stream';
+import { pipeline } from 'stream/promises';
+
+const transform = new Transform({
+  transform(chunk, _, callback) {
+    const reversed = String(chunk).split('').reverse().join('');
+    callback(null, reversed);
+  },
+});
+//v1
+const transform1 = async () => {
+  const stdin = process.stdin;
+  const stdout = process.stdout;
+
+  stdin.pipe(transform).pipe(stdout);
 };
 
-await transform();
+//v2
+const transform2 = async () => {
+  await pipeline(process.stdin, transform, process.stdout);
+};
+
+await transform1();
